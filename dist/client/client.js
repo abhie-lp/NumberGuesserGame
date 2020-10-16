97ca9255ef2f751027b60974d456d02cca76757a
Abhishek Tiwari <abhie.lp@gmail.com>
2020-10-16 16:37:19 +0530

    CSS and show hide panel

--- dist/client/client.js
+++ dist/client/client.js
@@ -9,6 +9,25 @@ var Client = /** @class */ (function () {
             }, 1000);
         });
     }
+    Client.prototype.showGame = function (id) {
+        switch (id) {
+            case 0:
+                $("#gamePanel1").fadeOut(100);
+                $("#gamePanel2").fadeOut(100);
+                $("#gamePanel0").delay(100).fadeIn(100);
+                break;
+            case 1:
+                $("#gamePanel0").fadeOut(100);
+                $("#gamePanel2").fadeOut(100);
+                $("#gamePanel1").delay(100).fadeIn(100);
+                break;
+            case 2:
+                $("#gamePanel0").fadeOut(100);
+                $("#gamePanel1").fadeOut(100);
+                $("#gamePanel2").delay(100).fadeIn(100);
+                break;
+        }
+    };
     return Client;
 }());
 var client = new Client();
--- src/client/client.ts
+++ src/client/client.ts
@@ -12,6 +12,26 @@ class Client {
       }, 1000)
     })
   }
+
+  showGame(id: number) {
+    switch (id) {
+      case 0:
+        $("#gamePanel1").fadeOut(100);
+        $("#gamePanel2").fadeOut(100);
+        $("#gamePanel0").delay(100).fadeIn(100);
+        break;
+      case 1:
+        $("#gamePanel0").fadeOut(100);
+        $("#gamePanel2").fadeOut(100);
+        $("#gamePanel1").delay(100).fadeIn(100);
+        break;
+      case 2:
+        $("#gamePanel0").fadeOut(100);
+        $("#gamePanel1").fadeOut(100);
+        $("#gamePanel2").delay(100).fadeIn(100);
+        break;
+    }
+  }
 }
 
 const client = new Client();
